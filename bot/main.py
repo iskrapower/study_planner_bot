fceb7b11635ca4554c1a7b907dcf9adbcc5e826c
Ilkham <ilkham200627@gmail.com>
2026-06-14 05:40:26 +0500

    Added first code

---
+++ bot/main.py
@@ -0,0 +1,15 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
