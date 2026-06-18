dd802e9d4cb749c64d92b9c90d78d4cff33e55e9
Ilkham <ilkham200627@gmail.com>
2026-06-18 09:53:20 +0500

    Updated planner.py

--- bot/handlers/planner.py
+++ bot/handlers/planner.py
@@ -1,24 +1,97 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+
+from states.planner_states import PlannerStates
 
 
 router = Router()
 
 
 @router.message(Command("plan"))
-async def plan_handler(message: Message):
+async def plan_handler(
+        message: Message,
+        state: FSMContext
+):
+
+    await state.set_state(
+        PlannerStates.subject
+    )
 
     await message.answer(
-        """
-            Let's create a preparation plan.
+        "What subject do you want to study?"
+    )
 
-            Write the subject.
 
-            For example:
+@router.message(PlannerStates.subject)
+async def subject_handler(
+        message: Message,
+        state: FSMContext
+):
 
-            Python
-            Mathematics
-            IELTS
+    await state.update_data(
+        subject=message.text
+    )
+
+    await state.set_state(
+        PlannerStates.exam_date
+    )
+
+    await message.answer(
+        "When is your exam date?"
+    )
+
+
+@router.message(PlannerStates.exam_date)
+async def exam_date_handler(
+        message: Message,
+        state: FSMContext
+):
+
+    await state.update_data(
+        exam_date=message.text
+    )
+
+    await state.set_state(
+        PlannerStates.daily_hours
+    )
+
+    await message.answer(
+        "How many hours can you study per day?"
+    )
+
+
+@router.message(PlannerStates.daily_hours)
+async def hours_handler(
+        message: Message,
+        state: FSMContext
+):
+
+    await state.update_data(
+        daily_hours=message.text
+    )
+
+
+    data = await state.get_data()
+
+
+    await message.answer(
+        f"""
+            Your study plan settings:
+
+            Subject:
+            {data['subject']}
+
+            Exam date:
+            {data['exam_date']}
+
+            Daily hours:
+            {data['daily_hours']}
+
+            I will generate your plan soon.
         """
     )
+
+
+    await state.clear()
