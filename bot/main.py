1e0c8bf227b8791ab219582dec99e14a2362c678
Ilkham <ilkham200627@gmail.com>
2026-06-18 09:39:27 +0500

    Made available commands in Telegram bot

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers.start import router
+from handlers.start import router as start_router
+from handlers.planner import router as planner_router
 
 bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher()
-dp.include_router(router)
+dp.include_router(start_router)
+dp.include_router(planner_router)
 
 
 async def main():
