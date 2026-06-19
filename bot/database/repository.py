5df6809b1ac2551578c5372548d94ae944468026
Ilkham <ilkham200627@gmail.com>
2026-06-19 15:19:04 +0500

    Created condition for checking if user exists

---
+++ bot/database/repository.py
@@ -0,0 +1,36 @@
+from sqlalchemy import select
+
+from database.database import async_session
+from database.models import User
+
+
+async def get_or_create_user(
+    telegram_id: int,
+    username: str | None
+):
+
+    async with async_session() as session:
+
+        result = await session.execute(
+            select(User)
+            .where(User.telegram_id == telegram_id)
+        )
+
+        user = result.scalar_one_or_none()
+
+
+        if user is None:
+
+            user = User(
+                telegram_id=telegram_id,
+                username=username
+            )
+
+            session.add(user)
+
+            await session.commit()
+
+            await session.refresh(user)
+
+
+        return user
