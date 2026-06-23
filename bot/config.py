95d8b3704763eae074aae3163d88c52378811d89
Ilkham <ilkham200627@gmail.com>
2026-06-23 11:53:28 +0500

    Added gemini model

--- bot/config.py
+++ bot/config.py
@@ -6,3 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL = os.getenv(
+    "GEMINI_MODEL",
+    "gemini-2.5-flash"
+)
